7a5248967f21774e33e856311ec5d09a33552cf8
Developer-Ivanov <Developer-Ivanov@users.noreply.github.com>
2017-02-08 16:18:53 +0700

    [XSS] in [Xss.aspx.cs]

--- AI_Issue_cross-site_scripting/AI_Issue_cross-site_scripting/Tests/1 INPUT DATA VERIFICATION/3 Cross-site Scripting/Xss.aspx.cs
+++ AI_Issue_cross-site_scripting/AI_Issue_cross-site_scripting/Tests/1 INPUT DATA VERIFICATION/3 Cross-site Scripting/Xss.aspx.cs
@@ -17,7 +17,7 @@ namespace Mopas.Tests
             string name = Request.Params["name"];
             //string name = Request.Params["name"];  //Request.Unvalidated().Params["name"];
             
-            // TODO: AI issue #65, High, Cross-site Scripting, vstfs:///WorkItemTracking/WorkItem/65
+            // FIXED: AI issue #65, High, Cross-site Scripting, vstfs:///WorkItemTracking/WorkItem/65
             // GET /Tests/1%20INPUT%20DATA%20VERIFICATION/3%20Cross-site%20Scripting/Xss.aspx?name=%3cscript%3ealert(1)%3c%2fscript%3e HTTP/1.1
             // Host: localhost
             Response.Write("<b>Hello, " + name + "!</b>");
